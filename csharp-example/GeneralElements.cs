62d89af9a0367fdcf9af9bd1b5b3cb13406333e2
AlenaZborovskaya <ezborovskaya@EZBOROVSKAYA>
2019-02-14 13:30:56 +0300

    Задание 8 (stickers_test)_попытка1

---
+++ csharp-example/GeneralElements.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+
+namespace QAFramework.Elements
+{
+    public class GeneralElements 
+    {
+        private IWebDriver driver;
+
+        public GeneralElements(IWebDriver driver)
+        {
+        }
+
+        /// <summary>
+        /// Проверяет отображение элемента
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="locator"></param>
+        /// <returns></returns>
+        public bool IsElementPresent(By locator)
+        {
+            try
+            {
+                driver.FindElement(locator);
+                return true;
+            }
+            catch (NoSuchElementException ex)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Проверяет отображение элементов
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="locator"></param>
+        /// <returns></returns>
+        public bool AreElementsPresent(By locator)
+        {
+            return driver.FindElements(locator).Count > 0;
+        }
+    }
+}
---
+++ csharp-example/Stickers_test.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using QAFramework.Elements;
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class Stickers_test
+    {
+        private static IWebDriver driver;
+        private WebDriverWait wait;
+
+        GeneralElements elements = new GeneralElements(driver);
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://localhost/litecart/en/";
+
+            // Проверяем, что для каждого товара есть стикер
+            if (elements.AreElementsPresent(By.XPath("//*[@class='product column shadow hover-light']")) == true)
+            {
+                var stickers = driver.FindElements(By.XPath("//*[@class='product column shadow hover-light']"));
+                foreach (var element in stickers)
+                {
+                    wait.Until(ExpectedConditions.ElementExists(By.XPath("//*[contains(@class,'sticker')]")));
+                }
+            }
+        }
+
+        [OneTimeTearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
